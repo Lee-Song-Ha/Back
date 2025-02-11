7d3a4763570c11b931294a05d332399bc8f93b8c
mnzy412 <mnzy412@gmail.com>
2025-02-12 05:49:24 +0900

    .

--- src/main/java/com/boot/swlugweb/v1/security/SecurityConfig.java
+++ src/main/java/com/boot/swlugweb/v1/security/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-
+//
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
