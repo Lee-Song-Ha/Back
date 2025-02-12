eeb56155dbea283345fe4b1217091cb905dd5926
mnzy412 <mnzy412@gmail.com>
2025-02-12 23:55:09 +0900

    [mnzy] 지원 페이지 - 기간 로직 추가

--- src/main/java/com/boot/swlugweb/v1/apply/ApplyController.java
+++ src/main/java/com/boot/swlugweb/v1/apply/ApplyController.java
@@ -1,16 +1,26 @@
 package com.boot.swlugweb.v1.apply;
 
 import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
 
 //회원가입
 @RestController
 @RequestMapping("/api/apply")
+@RequiredArgsConstructor
 public class ApplyController {
+
+//    지원페이지 무조건 열어두고 싶을 때
+//    @GetMapping
+//    public Boolean apply() {
+//
+//        return true;
+//    }
+
+    private final ApplyService applyService;  // final 키워드 추가
 
     @GetMapping
     public Boolean apply() {
-
-        return true;
+        return applyService.isApplicationPeriod();
     }
 
 }
